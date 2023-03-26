fba63b21655b5c1338bacd1eaa337a534a8065ac
KNishit92 <kumarnishit870@gmail.com>
2023-03-27 00:13:32 +0530

    patch and delete routes

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,8 +3,11 @@ const router = require('express').Router();
 
 // CRUD for posts by users
 router
-    .get('/posts', controller.getAll)
-    .get('/posts/:id', controller.getOne)
-    .post('/posts', controller.createOne)
-    .delete('/posts/:id', controller.deleteOne)
-        
+    .get('/posts', controller.getAll) // all posts by all the users
+    .get('users/:id/posts/:id', controller.getOne) // specific post by a specific user
+    .get('/users/:id/posts', controller.getAll) // get all posts by a user
+    .post('/users/:id/posts', controller.createOne)
+    .patch('/users/:id/posts/:id', controller.patch)
+    .delete('/users/:id/posts/:id', controller.deleteOne)
+    .delete('/users/:id/posts', controller.delete) // delete all posts by a user 
+
